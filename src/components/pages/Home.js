685f868bb1ce1db723b225b0e7f3248756b7d5fa
Nikolay <niknikpopov@gmail.com>
2021-05-19 22:13:10 +0300

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {useDispatch,useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function Home(props) {
-    const dispatch=useDispatch();
-    const selector=useSelector(state => state.btn.button);
+    const dispatch = useDispatch();
+    const btnState = useSelector(state => state.btn.button);
     return (
         <div style={{textAlign:'center'}}>
             <h2>Home</h2>
-            <button onClick={()=>dispatch({type:"click"})}>
-                {selector ? 'Open' :"Close"}
+            <button onClick={() = >dispatch({type:"click"})}>
+                {btnState ? 'Open' :"Close"}
             </button>
         </div>
     );

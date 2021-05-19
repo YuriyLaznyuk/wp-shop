f25c77f7e4cc29771e600d183d7ae65cb425ccae
Nikolay <niknikpopov@gmail.com>
2021-05-19 22:11:36 +0300

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,19 @@ function About(props) {
     return (
         <div>
             <h2 style={{textAlign:'center'}}>About</h2>
-            <StopWatch/>
+            <p>
+                "Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit,    
+                sed do eiusmod tempor incididunt ut 
+                labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, 
+                quis nostrud exercitation ullamco 
+                laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate 
+                velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
         </div>
     );
 }
